66f79ba719d06689ec329b4d094260bb8b19ebaa
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-04-05 12:30:06 -0700

    El slider ya es responsivo con el audio que escucha.

--- Entrada/MainWindow.xaml.cs
+++ Entrada/MainWindow.xaml.cs
@@ -57,15 +57,42 @@ namespace Entrada
             waveIn = new WaveIn();
 
             //Formato de audio
-            waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+            waveIn.WaveFormat = new WaveFormat(44100, 16, 1); // 44100 = sample rate | 16 = bit depth | 1 = canal
 
             //Buffer
             waveIn.BufferMilliseconds = 250;
 
-            //
+            //¿Qué hacer cuando hay muestras disponibles?
+            waveIn.DataAvailable += WaveIn_DataAvailable;
 
+            waveIn.StartRecording();
         }
 
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+            float acumulador = 0.0f;
+
+            for (int i = 0; i < bytesGrabados; i+=2)
+            {
+                // TRANSFORMANDO 2 BYTES SEPARADOS en una muesstra de 16 bits:
+                // 1. Toma el segundo byte y le antepone ocho 0's al principio.
+                // 2. Hace un OR con el primer byte al cual automaticamente se le llenan ocho 0's al final.
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+
+                float muestra32bits = muestra / 32768.9f;
+                acumulador += Math.Abs(muestra32bits);
+            }
+
+            float promedio = acumulador / (bytesGrabados / 2);
+            sldMicrofono.Value = (double)promedio;
+        }
+
+        private void BtnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
+        }
     }
 
 }
